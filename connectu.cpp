7f516603c280bd8d26e051d316e34d58553201bd
zhupeili-uh <zhupeili@hawaii.edu>
2026-01-18 20:57:49 -1000

    Initial commit

--- connectu.cpp
+++ connectu.cpp
@@ -8,7 +8,7 @@
  * if your Lab 1 implementation is incomplete.
  * * ONLY uncomment saveData() after you have verified Lab 1 works!
  */
-
+//Simple edit for initial commit.
 #include <iostream>
 #include <string>
 #include <fstream>
